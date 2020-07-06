8fcfa70885ac185c8e7c32628b8db371129b1721
Dhriti Singh <64511837+Dhriti-singh@users.noreply.github.com>
2020-07-07 04:14:52 +0530

    Create singleCellDraw.js

---
+++ src/Grid/singleCellDraw.js
@@ -0,0 +1,26 @@
+let canvas = document.getElementById('canvas_trigger');
+// x = column number
+// y = row number
+function singleCellDraw(x,y,color){
+   let ctx = canvas.getContext('2d');
+   const width = canvas.getBoundingClientRect().width;
+   const height = canvas.getBoundingClientRect().height;
+   let cellWidth = (width/cols) ;
+   let cellHeight =(height/rows);
+   //  console.log(i);
+   //  console.log(j);
+   let x1 =  y* cellWidth;
+   let y1 =  x* cellHeight;
+   let x2 =  cellWidth;
+   let y2 =  cellHeight;
+   //Set the line and fill style options
+   ctx.strokeStyle = "black";
+   ctx.lineWidth = 3;
+   ctx.fillStyle = color;
+   //Draw the rectangle
+   ctx.beginPath();
+   ctx.rect(x1, y1, x2, y2);
+   ctx.stroke();
+   ctx.fill();
+   drawGrid(rows,cols, canvas);
+}
