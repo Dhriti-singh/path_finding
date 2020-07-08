5a25b316f7f9aa2b0d734b00881e09ebef01b9b8
Dhriti Singh <64511837+Dhriti-singh@users.noreply.github.com>
2020-07-08 21:11:42 +0530

    BFS added
    bactracking implementation

--- src/Algorithms/BFS.js
+++ src/Algorithms/BFS.js
@@ -1,4 +1,4 @@
-// //each probelm needs to have
+// each probelm needs to have
 // - initial state
 // -actions
 // -transition state
@@ -26,9 +26,11 @@
 //  - --if they aren't already in the frontier or in the explored state
 
 //the frontier stores the nodes to be expanded;
-function call_DFS(){
-	let nodes = [[]];
 
+
+function call_BFS(){
+	let nodes = [[]];
+	//defining the state of every node in BFS
 	for(let i=0;i<rows;i++){
 		nodes[i] =[];
 		for(let j=0;j<cols;j++){
@@ -38,15 +40,15 @@ function call_DFS(){
 		}
 	}
 
+
 	let frontier = [];
 	frontier.push(initialState);
 
-	//implementing it using a stack
 	let shortestPath = [];
 	let visitedNodes = [];
 	let exploredNodes = [];
 
-	let dfsFinish = false;
+	let bfsFinish = false;
 
 	function check( currentNode ){
 	 	if(currentNode.i == goalState.i && currentNode.j == goalState.j)
@@ -55,9 +57,6 @@ function call_DFS(){
 	 		return false;
 	}
 
-	//need to reset everything after running one algorihtm,
-	//the explored nodes sepecially
-
 	for(let i=0;i<rows;i++){
 		exploredNodes[i]=[];
 		for(let j=0;j<cols;j++){
@@ -65,24 +64,20 @@ function call_DFS(){
 		}
 	}
 
-	function DFS(){
-		if(dfsFinish){
-			return;
+	while(frontier.length > 0){
+		if(bfsFinish){
+			break;
 		}
-		if(frontier.lenght==0){
-			console.log("no solution available");
-			return;
+		let front = frontier.shift();
+
+		if(check(front)){
+			bfsFinish = true;
+			console.log("BFS finsished");
+			break;
 		}
 
-		let front = frontier.pop();
-		if(check( front) ){
-			dfsFinish = true;
-			console.log("DFS finished");
-			return;
-		}
-
-		exploredNodes[front.i][front.j]=1;
-		visitedNodes.push(new selectedNode(front.i, front.j));
+		exploredNodes[front.i][front.j] = 1;
+		visitedNodes.push(new selectedNode(front.i , front.j));
 
 		if(front.j>0){
 			if(exploredNodes[front.i][front.j-1]==0 && nodes[front.i][front.j-1].walk==0){
@@ -90,7 +85,6 @@ function call_DFS(){
 				frontier.push(nodes[front.i][front.j-1]);
 				nodes[front.i][front.j-1].parentX = front.i;
 				nodes[front.i][front.j-1].parentY = front.j;
-				DFS();
 			}
 		}
 		if(front.i>0){
@@ -99,7 +93,6 @@ function call_DFS(){
 				frontier.push(nodes[front.i-1][front.j]);
 				nodes[front.i-1][front.j].parentX = front.i;
 				nodes[front.i-1][front.j].parentY = front.j;
-				DFS();
 			}
 		}
 		if(front.i<rows-1){
@@ -108,7 +101,6 @@ function call_DFS(){
 				frontier.push(nodes[front.i+1][front.j]);
 				nodes[front.i+1][front.j].parentX = front.i;
 				nodes[front.i+1][front.j].parentY = front.j;
-				DFS();
 			}
 		}
 		if(front.j<cols-1){
@@ -117,14 +109,12 @@ function call_DFS(){
 				frontier.push(nodes[front.i][front.j+1]);
 				nodes[front.i][front.j+1].parentX = front.i;
 				nodes[front.i][front.j+1].parentY = front.j;
-				DFS();
 			}
 		}
-	}
-	DFS();
 
-	if(dfsFinish===false){
-		console.log("no solution available");
+	}
+	if(bfsFinish===false){
+		console.log("no solution");
 	}
 	else{
 		let currX = goalState.i;
@@ -134,6 +124,7 @@ function call_DFS(){
 				break;
 			}
 			else{
+				//console.log( "in time" , currX, currY);
 				let parent_x = nodes[currX][currY].parentX;
 				let parent_y = nodes[currX][currY].parentY;
 				//console.log(parent_x, parent_y);
@@ -143,11 +134,9 @@ function call_DFS(){
 				shortestPath.push(curr);
 			}
 		}
-
 	}
 	drawArrayBlue(visitedNodes);
 	drawArrayYellow(shortestPath);
-	singleCellDraw(initialState.i, initialState.j,"green");
+	singleCellDraw(initialState.i,initialState.j,"green");
 	singleCellDraw(goalState.i,goalState.j,"red");
-
 }
